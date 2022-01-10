7f50e8b7ed95b6c3e869022a3427b279454fb59c
skewballfox <joshua.ferguson.273@gmail.com>
2022-01-10 11:54:19 -0600

    refactor of analytics feature complete, requires further testing before DoD

--- get_base_model_flow.py
+++ get_base_model_flow.py
@@ -58,33 +58,40 @@ def split_data_and_get_best_model_flow(
 
     print(dialog_handler_instance.render_template("inform-experiment_train_test_split"))
     precise_modeling_operations_instance.run_experimental_training(model_names)
+    # TODO: use model_analytics class to get the best model
     (
-        average_val_acc,
-        standard_deviation_val_acc,
-        average_acc,
-        standard_deviation_acc,
-    ) = precise_modeling_operations_instance.get_models_analytics()
-    # TODO: change to f1 score
+        average_acc_for_min_loss_models,
+        stdev_acc_for_min_loss_models,
+        average_val_acc_for_min_loss_models,
+        stdev_val_acc_for_min_loss_models,
+    ) = precise_modeling_operations_instance.get_optimal_training_model_analytics()
+    # average_val_acc, standard_deviation_val_acc, average_acc, standard_deviation_acc = precise_modeling_operations_instance.get_models_analytics()
     experimental_average_accuracy_dialog = dialog_handler_instance.render_template(
         "inform-accuracy",
-        average_val_acc=average_val_acc,
-        standard_deviation_val_acc=standard_deviation_val_acc,
-        average_acc=average_acc,
-        standard_deviation_acc=standard_deviation_acc,
+        average_val_acc=average_val_acc_for_min_loss_models,
+        standard_deviation_val_acc=stdev_val_acc_for_min_loss_models,
+        average_acc=average_acc_for_min_loss_models,
+        standard_deviation_acc=stdev_acc_for_min_loss_models,
     )
+    # TODO: change to f1 score
+    # experimental_average_accuracy_dialog = dialog_handler_instance.render_template("inform-accuracy", average_val_acc=average_val_acc, standard_deviation_val_acc=standard_deviation_val_acc, average_acc=average_acc, standard_deviation_acc=standard_deviation_acc)
     print(experimental_average_accuracy_dialog)
 
-    (
-        selected_model_name,
-        selected_model_results,
-    ) = precise_modeling_operations_instance.pick_best_model()
-    experimental_best_model = dialog_handler_instance.render_template(
+    best_training_set_accuracy_model = (
+        precise_modeling_operations_instance.pick_best_model()
+    )
+    # selected_model_name, selected_model_results = precise_modeling_operations_instance.pick_best_model()
+    # experimental_best_model = dialog_handler_instance.render_template("inform-best_model", selected_model_name=selected_model_name, selected_model_results=selected_model_results)
+    print(best_training_set_accuracy_model)
+    selected_model_name = list(best_training_set_accuracy_model.keys())[0]
+    selected_model_results = best_training_set_accuracy_model[selected_model_name]
+    experimental_average_accuracy_dialog = dialog_handler_instance.render_template(
         "inform-best_model",
         selected_model_name=selected_model_name,
         selected_model_results=selected_model_results,
     )
-    print(experimental_best_model)
-
+    # return (selected_model_name, selected_model_results, experimental_average_accuracy_dialog)
+    # return(best_training_set_accuracy_model)
     return (
         selected_model_name,
         selected_model_results,
@@ -109,7 +116,8 @@ def train_model_flow(wakeword_model_name, epochs=None):
         )
     )
 
-    precise_modeling_operations_instance.get_last_epoch_model_info(
+    # precise_modeling_operations_instance.get_last_epoch_model_info(wakeword_model_name, training_run)
+    precise_modeling_operations_instance.get_model_info(
         wakeword_model_name, training_run
     )
 
@@ -250,3 +258,43 @@ model_info = train_model_flow(wakeword_model_name, epochs)
 print(model_info)
 model_info = incremental_training_flow(noise_destination_directory, wakeword_model_name)
 print(model_info)"""
+
+"""
+import json
+
+with open('data_prep_user_configuration.json', 'r') as file:
+    user_configuration_dictionary = json.load(file)
+source_directory = user_configuration_dictionary['audio_source_directory']
+wakeword_model_name = user_configuration_dictionary['wakeword_model_name']
+pdsounds_directory = user_configuration_dictionary['pdsounds_directory']
+directories_to_process = user_configuration_dictionary['extra_audio_directories_to_process']
+extra_audio_directories_labels = user_configuration_dictionary['extra_audio_directories_labels']
+max_files_from_source_directory = user_configuration_dictionary['max_files_from_source_directory']
+max_files_per_destination_directory = user_configuration_dictionary['max_files_per_destination_directory']
+
+
+with open('data_prep_system_configuration.json', 'r') as file:
+    system_configuration_dictionary = json.load(file)    
+random_split_directories = system_configuration_dictionary['random_split_directories']
+even_odd_split_directories = system_configuration_dictionary['even_odd_split_directories']
+three_four_split_directories = system_configuration_dictionary['three_four_split_directories']
+root_model_name = system_configuration_dictionary['root_model_name']
+source_directories = system_configuration_dictionary['source_directories']
+destination_directories = system_configuration_dictionary['destination_directories']
+directories_to_gauss = system_configuration_dictionary['directories_to_gauss']
+
+best_training_set_accuracy_model = split_data_and_get_best_model_flow(source_directory, random_split_directories, even_odd_split_directories, three_four_split_directories, root_model_name)
+#print(best_training_set_accuracy_model)
+print(best_training_set_accuracy_model)
+"""
+
+
+"""
+changed experiment_4 to test_wakeword_model_delete_after
+Average accuracies of the base model...
+experiment_4 produces the best results with {'minimum_loss_val_accuracy': {'epoch': 49, 'loss': 0.1925, 'acc': 0.6705, 'val_loss': 0.1503, 'val_acc': 0.6949, 'acc_difference': 0.024399999999999977}}
+Original best model: {'minimum_loss_val_accuracy': {'epoch': 49, 'loss': 0.1925, 'acc': 0.6705, 'val_loss': 0.1503, 'val_acc': 0.6949, 'acc_difference': 0.024399999999999977}}
+Current base model with incremental training on your random audio recordings: {'acc': 0.6878, 'val_acc': 0.6901, 'difference': 0.0023000000000000798}
+Not bad when you consider the average accuracies of the first base model...
+"""
+# TODO: the produces the best results with is wrong, it gives minium_loss_val_acc instead of minimum_loss_acc
--- model_analytics.py
+++ model_analytics.py
@@ -1,150 +1,152 @@
-import json
-
-# Open the json file and load the content
-with open('training_run.json', 'r') as file:
-    training_run_json = json.load(file)
-
-# string matches for the lines we want to scrape
-def scrape_epoch_training_results(training_run_json):
-    training_complete_string = '[==============================]'
-    epoch_number = 'Epoch'
-    #TODO: add in the category counts and return them
-    category_counts = 'Data:'
-
-    epochs = [line for line in training_run_json if epoch_number in line]
-
-    epoch_training_results = [line.split(' - ')[2:] for line in training_run_json if training_complete_string in line]
-    return zip(epoch_training_results, epochs)
-
-
-
-def transform_epoch_number_and_accuracy(training_run_json):
-    epochs_accuracy = {}
-    epochs_training_results = scrape_epoch_training_results(training_run_json)
-    for epoch_result, epoch_number in epochs_training_results:
-        epoch_number = int(epoch_number.strip('Epoch ').rsplit('/', 1)[0])
-        loss = float(epoch_result[0].strip('loss: '))
-        acc = float(epoch_result[1].strip('acc: '))
-        val_loss = float(epoch_result[2].strip('val_loss: '))
-        val_acc = float(epoch_result[3].strip('val_acc: '))
-
-        accuracy_dict = {
-            'epoch': epoch_number,
-            'loss': loss,
-            'acc': acc,
-            'val_loss': val_loss,
-            'val_acc': val_acc,
-            'acc_difference': abs(acc - val_acc)
-            }
-
-        epochs_accuracy[epoch_number] = accuracy_dict
-    return epochs_accuracy
-
-
-model_name = 'experiment_model_1'
-
-def get_maximum_accuracy(training_run_json):
-    epochs_accuracy = transform_epoch_number_and_accuracy(training_run_json)
-    minimum_loss_accuracy = min(epochs_accuracy.values(), key=lambda x: x['loss'])
-    minimum_loss_val_accuracy = min(epochs_accuracy.values(), key=lambda x: x['val_loss'])
-
-    return (minimum_loss_accuracy, minimum_loss_val_accuracy)
-
-minimum_loss_accuracy, minimum_loss_val_accuracy = get_maximum_accuracy(training_run_json)
-#print(minimum_loss_accuracy)
-#print(minimum_loss_val_accuracy)
-
-models = {}
-
-def add_model(model_name, training_run_json):
-    minimum_loss_accuracy, minimum_loss_val_accuracy = get_maximum_accuracy(training_run_json)
-    models = {}
-    models[model_name] = [
-    {
-        'minimum_loss_accuracy': minimum_loss_accuracy
-        },
-    {
-       'minimum_loss_val_accuracy': minimum_loss_val_accuracy
-        }
-        ]
-    return models
-
-models = add_model(model_name, training_run_json)
-
-models['experiment_model_2'] = [
-    {
-        'minimum_loss_accuracy': {
-            'epoch': 15,
-            'loss': 0.11,
-            'acc': 0.9,
-            'val_loss': 0.12,
-            'val_acc': 0.89,
-            'acc_difference': abs(0.9 - 0.89)
-        },
-    },
-    {
-        'minimum_loss_val_accuracy': {
-            'epoch': 16,
-            'loss': 0.12,
-            'acc': 0.89,
-            'val_loss': 0.15,
-            'val_acc': 0.62,
-            'acc_difference': abs(0.89 - 0.91)
-            }
-        }
-        
-]
-
-
-#print(models)
-
-
-
-# This is the part that calculates the smallest minimum_loss and minimum_loss_val_acc for all the models
-
-def get_model_accuracies(models):
-    acc_for_min_loss_models = [item[1][0]['minimum_loss_accuracy']['acc'] for item in models.items()]
-    val_acc_for_min_val_loss_models = [item[1][1]['minimum_loss_val_accuracy']['val_acc'] for item in models.items()]
-    return (acc_for_min_loss_models, val_acc_for_min_val_loss_models)
-
-def get_max_accuracies_over_all_models(models):
-    acc_for_min_loss_models, val_acc_for_min_val_loss_models = get_model_accuracies(models)
-    max_acc_for_min_loss_model = max(acc_for_min_loss_models)
-    max_val_acc_for_min_val_loss_model = max(val_acc_for_min_val_loss_models)
-    return (max_acc_for_min_loss_model, max_val_acc_for_min_val_loss_model)
-
-#print(get_max_accuracies_over_all_models(models))
-
 import statistics
 
-def get_model_analytics(models):
-    acc_for_min_loss_models, val_acc_for_min_val_loss_models = get_model_accuracies(models)
-    return (statistics.mean(acc_for_min_loss_models), statistics.stdev(acc_for_min_loss_models) , statistics.mean(val_acc_for_min_val_loss_models), statistics.stdev(val_acc_for_min_val_loss_models))
+"""# Open the json file and load the content
+with open('training_run.json', 'r') as file:
+    training_run_json = json.load(file)"""
 
-#print(get_model_analytics(models))
 
-def get_best_models(models):
-    max_acc_for_min_loss_model, max_val_acc_for_min_val_loss_model = get_max_accuracies_over_all_models(models)
-    best_models = []
-    max_acc_for_min_loss_model_dict = {}
-    max_val_acc_for_min_val_loss_model_dict = {}
-    for item in models.items():
-        if item[1][0]['minimum_loss_accuracy']['acc'] == max_acc_for_min_loss_model:
-            max_acc_for_min_loss_model_dict[item[0]] = item[1][0]
-            best_models.append(max_acc_for_min_loss_model_dict)
-        if item[1][1]['minimum_loss_val_accuracy']['val_acc'] == max_val_acc_for_min_val_loss_model:
-            max_val_acc_for_min_val_loss_model_dict[item[0]] = item[1][1]
-            best_models.append(max_val_acc_for_min_val_loss_model_dict)
-    return best_models
+class ModelAnalytics:
+    def __init__(self):
+        self.models = {}
 
-best_models = get_best_models(models)
+    # string matches for the lines we want to scrape
+    @staticmethod
+    def scrape_epoch_training_results(training_run_json):
+        training_complete_string = '[==============================]'
+        epoch_number = 'Epoch'
+        #TODO: add in the category counts and return them
 
-# This is used when the training is run for -sb with the loss being the measure (default)
-def get_best_max_acc_for_min_loss_model(best_models):
-    max_acc_for_min_loss_model_dict = best_models[0]
-    return max_acc_for_min_loss_model_dict
+        epochs = [line for line in training_run_json if epoch_number in line]
 
-# This is used when the training is run for -sb with the val_loss being the measure (important to optimize when the test set is big enough)
-def get_best_max_val_acc_for_min_val_loss_model(best_models):
-    max_val_acc_for_min_val_loss_model_dict = best_models[1]
-    return max_val_acc_for_min_val_loss_model_dict
+        epoch_training_results = [line.split(' - ')[2:] for line in training_run_json if training_complete_string in line]
+        return zip(epoch_training_results, epochs)
+
+    @staticmethod
+    def scrape_category_counts(training_run_json):
+        category_counts_string = 'Data:'
+        for line in training_run_json:
+            if category_counts_string in line:
+                category_counts = line.split(' ')
+                return category_counts
+
+    def transform_category_counts(self, training_run_json):
+        category_counts = self.scrape_category_counts(training_run_json)
+        wake_words_count = int(category_counts[2].strip('wake_words='))
+        not_wake_words_count = int(category_counts[3].strip('not_wake_words='))
+        test_wake_words_count = int(category_counts[4].strip('test_wake_words='))
+        test_not_wake_words_count = int(category_counts[5].strip('test_not_wake_words=').strip('>'))
+        category_counts_dict = {
+            'training_data':
+        {
+            'wake_words': wake_words_count,
+            'not_wake_words': not_wake_words_count,
+            'test_wake_words': test_wake_words_count,
+            'test_not_wake_words': test_not_wake_words_count
+        }
+        }
+        return category_counts_dict
+
+
+
+    def transform_epoch_number_and_accuracy(self, training_run_json):
+        epochs_accuracy = {}
+        epochs_training_results = self.scrape_epoch_training_results(training_run_json)
+        for epoch_result, epoch_number in epochs_training_results:
+            epoch_number = int(epoch_number.strip('Epoch ').rsplit('/', 1)[0])
+            loss = float(epoch_result[0].strip('loss: '))
+            acc = float(epoch_result[1].strip('acc: '))
+            val_loss = float(epoch_result[2].strip('val_loss: '))
+            val_acc = float(epoch_result[3].strip('val_acc: '))
+
+            accuracy_dict = {
+                'epoch': epoch_number,
+                'loss': loss,
+                'acc': acc,
+                'val_loss': val_loss,
+                'val_acc': val_acc,
+                'acc_difference': abs(acc - val_acc)
+                }
+
+            epochs_accuracy[epoch_number] = accuracy_dict
+        return epochs_accuracy
+
+    def get_maximum_accuracy(self, training_run_json):
+        epochs_accuracy = self.transform_epoch_number_and_accuracy(training_run_json)
+        minimum_loss_accuracy = min(epochs_accuracy.values(), key=lambda x: x['loss'])
+        minimum_loss_val_accuracy = min(epochs_accuracy.values(), key=lambda x: x['val_loss'])
+
+        return (minimum_loss_accuracy, minimum_loss_val_accuracy)
+
+    def add_model(self, model_name, training_run_json):
+        category_counts_dict = self.transform_category_counts(training_run_json)
+        minimum_loss_accuracy, minimum_loss_val_accuracy = self.get_maximum_accuracy(training_run_json)
+
+  
+        self.models[model_name] = [
+        {
+            'minimum_loss_accuracy': minimum_loss_accuracy
+            },
+        {
+        'minimum_loss_val_accuracy': minimum_loss_val_accuracy
+            },
+            category_counts_dict
+            ]
+        return self.models
+
+
+
+    # This is the part that calculates the smallest minimum_loss and minimum_loss_val_acc for all the models
+    # This will have to be changed to get the self.models from PreciseModelingOperations
+    def get_model_accuracies(self):
+        acc_for_min_loss_models = [item[1][0]['minimum_loss_accuracy']['acc'] for item in self.models.items()]
+        val_acc_for_min_loss_models = [item[1][0]['minimum_loss_accuracy']['val_acc'] for item in self.models.items()]
+        #TODO: add val_acc_for_min_loss_models, acc_for_min_val_loss_models
+        acc_for_min_val_loss_models = [item[1][1]['minimum_loss_val_accuracy']['acc'] for item in self.models.items()]
+        val_acc_for_min_val_loss_models = [item[1][1]['minimum_loss_val_accuracy']['val_acc'] for item in self.models.items()]
+        
+        return (acc_for_min_loss_models, val_acc_for_min_loss_models, acc_for_min_val_loss_models, val_acc_for_min_val_loss_models)
+
+    def get_max_accuracies_over_all_models(self):
+        acc_for_min_loss_models, val_acc_for_min_loss_models, acc_for_min_val_loss_models, val_acc_for_min_val_loss_models = self.get_model_accuracies()
+        max_acc_for_min_loss_model = max(acc_for_min_loss_models)
+        
+        #TODO: get same model value of val_acc_for_min_loss_models
+        max_val_acc_for_min_val_loss_model = max(val_acc_for_min_val_loss_models)
+        #TODO: get same model value of acc_for_min_val_loss_models
+        return (max_acc_for_min_loss_model, max_val_acc_for_min_val_loss_model)
+
+    #print(get_max_accuracies_over_all_models(models))
+
+    def get_model_analytics(self):
+        acc_for_min_loss_models, val_acc_for_min_loss_models, val_acc_for_min_val_loss_models, acc_for_min_val_loss_models = self.get_model_accuracies()
+        return (statistics.mean(acc_for_min_loss_models), statistics.stdev(acc_for_min_loss_models), statistics.mean(val_acc_for_min_loss_models), statistics.stdev(val_acc_for_min_loss_models), statistics.mean(acc_for_min_val_loss_models), statistics.stdev(acc_for_min_val_loss_models), statistics.mean(val_acc_for_min_val_loss_models), statistics.stdev(val_acc_for_min_val_loss_models))
+
+    #print(get_model_analytics(models))
+
+    def get_best_models(self):
+        max_acc_for_min_loss_model, max_val_acc_for_min_val_loss_model = self.get_max_accuracies_over_all_models()
+        best_models = []
+        max_acc_for_min_loss_model_dict = {}
+        max_val_acc_for_min_val_loss_model_dict = {}
+        for item in self.models.items():
+            if item[1][0]['minimum_loss_accuracy']['acc'] == max_acc_for_min_loss_model:
+                max_acc_for_min_loss_model_dict[item[0]] = item[1][0]
+                best_models.append(max_acc_for_min_loss_model_dict)
+            if item[1][1]['minimum_loss_val_accuracy']['val_acc'] == max_val_acc_for_min_val_loss_model:
+                max_val_acc_for_min_val_loss_model_dict[item[0]] = item[1][1]
+                best_models.append(max_val_acc_for_min_val_loss_model_dict)
+        return best_models
+
+    """best_models = get_best_models(models)"""
+
+    # This is used when the training is run for -sb with the loss being the measure (default)
+    @staticmethod
+    def get_best_max_acc_for_min_loss_model(best_models):
+        max_acc_for_min_loss_model_dict = best_models[0]
+        return max_acc_for_min_loss_model_dict
+
+    # This is used when the training is run for -sb with the val_loss being the measure (important to optimize when the test set is big enough)
+    @staticmethod
+    def get_best_max_val_acc_for_min_val_loss_model(best_models):
+        max_val_acc_for_min_val_loss_model_dict = best_models[1]
+        return max_val_acc_for_min_val_loss_model_dict
